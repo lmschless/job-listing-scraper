df6ce0195db2e2b3a9d833347be31c166a263549
lmschless <17575298+lmschless@users.noreply.github.com>
2024-03-13 19:08:43 -0700

    small change

--- job_scraper.py
+++ job_scraper.py
@@ -4,7 +4,7 @@ import yagmail
 from bs4 import BeautifulSoup
 
 def scrape_indeed(search_terms, location):
-    base_url = "https://www.usajobs.gov?" # updated from indeed
+    base_url = "https://www.usajobs.gov?" # updated from indeedd
     params = {
         "q": search_terms,
         "l": location
